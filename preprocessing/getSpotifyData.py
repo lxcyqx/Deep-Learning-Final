2f015f509067cb67aec2f72e90ec856507d7738d
Tin Tin <tintin@tins-air.devices.brown.edu>
2021-11-18 18:48:30 -0500

    preprocessing wrote lovePlaylist data

--- preprocessing/getSpotifyData.py
+++ preprocessing/getSpotifyData.py
@@ -7,10 +7,6 @@ secret = '82a8f39ecffd4bc0ae1fb33fe5692131'
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-artist_name = []
-track_name = []
-popularity = []
-track_id = []
 # I need to search for the playlist I want 
 # use sp.search (to find specific playlist)
 
@@ -20,11 +16,24 @@ track_id = []
 # love playlist #4 Country Kind Of Love https://open.spotify.com/playlist/37i9dQZF1DX8WMG8VPSOJC
 # love playlist #5 Love songs https://open.spotify.com/playlist/7BqzGVMQ21rr63avfYYnn0
 # love playlist #6 Rock Love Songs https://open.spotify.com/playlist/37i9dQZF1DX7Z7kYpKKGTc
+
 songUniqueIds = set() 
 
-
 lovePlaylistOneTracks = sp.playlist_tracks("https://open.spotify.com/playlist/2p6SAq9WHFaodqizQ0TTdO", additional_types=('track', ))
+lovePlaylistTwoTracks = sp.playlist_tracks("https://open.spotify.com/playlist/37i9dQZF1DX7rOY2tZUw1k", additional_types=('track', ))
+lovePlaylistThreeTracks = sp.playlist_tracks("https://open.spotify.com/playlist/7tj5Dblki89mIdHVzIm6DO", additional_types=('track', ))
+lovePlaylistFourTracks = sp.playlist_tracks("https://open.spotify.com/playlist/37i9dQZF1DX8WMG8VPSOJC", additional_types=('track', ))
+lovePlaylistFiveTracks = sp.playlist_tracks("https://open.spotify.com/playlist/7BqzGVMQ21rr63avfYYnn0", additional_types=('track', ))
+lovePlaylistSixTracks = sp.playlist_tracks("https://open.spotify.com/playlist/37i9dQZF1DX7Z7kYpKKGTc", additional_types=('track', ))
+
 lovePlaylistOneTrackIds = getTrackIds(lovePlaylistOneTracks)
+lovePlaylistTwoTrackIds = getTrackIds(lovePlaylistTwoTracks)
+lovePlaylistThreeTrackIds = getTrackIds(lovePlaylistThreeTracks)
+lovePlaylistFourTrackIds = getTrackIds(lovePlaylistFourTracks)
+lovePlaylistFiveTrackIds = getTrackIds(lovePlaylistFiveTracks)
+lovePlaylistSixTrackIds = getTrackIds(lovePlaylistSixTracks)
+allLovePlaylistTrackIds = lovePlaylistOneTrackIds + lovePlaylistTwoTrackIds + lovePlaylistThreeTrackIds + lovePlaylistFourTrackIds + lovePlaylistFiveTrackIds + lovePlaylistSixTrackIds 
+lovePlaylistSongNameAndArtist = allTracksWithFeatures(allLovePlaylistTrackIds)
 
 
 def getTrackIds(playlistTracks):
@@ -35,7 +44,6 @@ def getTrackIds(playlistTracks):
             ids.append(track['id'])
     return ids
 
-
 def getTrackFeatures(id):
     meta = sp.track(id)
     name = meta['name']
@@ -43,39 +51,18 @@ def getTrackFeatures(id):
     track = [name, artist]
     return track
 
-
-
-
-tracks = []
-for i in range(len(ids)):
-    time.sleep(.5)
-    track = getTrackFeatures(ids[i])
-    tracks.append(track)
+def allTracksWithFeatures(playlistIds):
+    tracksWithFeature = [] 
+    for id in playlistIds:
+        trackFeatures = getTrackFeatures(id)
+        tracksWithFeature.append(trackFeatures)
+    return tracksWithFeature
+    
 
 # create dataset
-df = pd.DataFrame(tracks, columns = ['name', 'album', 'artist', 'release_date', 'length', 'popularity', 'danceability', 'acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'loudness', 'speechiness', 'tempo', 'time_signature'])
-df.to_csv("spotify.csv", sep = ',')
+dfLovePlaylist = pd.DataFrame(lovePlaylistSongNameAndArtist , columns = ['name', 'artist'])
+dfLovePlaylist.to_csv("lovePlaylist.csv", sep = ',')
 
-
-
-# def getTrackIDs(user, playlist_id):
-#     ids = []
-#     playlist = sp.user_playlist(user, playlist_id)
-#     for item in playlist['tracks']['items']:
-#         track = item['track']
-#         ids.append(track['id'])
-#     return ids
-
-
-
-# for i in range(0,10000,50):
-#     track_results = sp.search(q='year:2018', type='track', limit=50,offset=i)
-#     for i, t in enumerate(track_results['tracks']['items']):
-#         artist_name.append(t['artists'][0]['name'])
-#         track_name.append(t['name'])2
-#         track_id.append(t['id'])
-#         popularity.append(t['popularity'])
-        
         
 # track_dataframe = pd.DataFrame({'artist_name' : artist_name, 'track_name' : track_name, 'track_id' : track_id, 'popularity' : popularity})
 # print(track_dataframe.shape)
