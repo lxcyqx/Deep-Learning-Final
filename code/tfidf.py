0dfc7a95a12b4462cc2af35186050331f8dbc953
Daniela Haidar <danihaidarjorge@gmail.com>
2021-12-10 00:02:16 -0500

    fix indexing error on tfidf

--- code/tfidf.py
+++ code/tfidf.py
@@ -1,14 +1,23 @@
 from collections import Counter
-# from getData import getRawData
-# from getSpotifyData import returnData
+from bs4 import BeautifulSoup
 import pandas as pd
-import nltk
+import os
+import re
 import numpy as np
 import sys
+import requests
 
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 from nltk.tokenize import RegexpTokenizer
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+#GENIUS API STUFF
+GENIUS_API_TOKEN='YcO4NV1shV8O0pA4kw9eCJflks0JpecSoMpU2v8sc2fitjDfKfHpGOClBJSAzstM'
 
 TITLE_IDX = 2
 LYRICS_IDX = 3
@@ -83,18 +92,60 @@ def find_closest_song(song_id, tf_idf_matrix):
                 similar_song_id = i
     return similar_song_id, min_dist
 
+
+def scrape_song_lyrics(url):
+
+    try:
+        page = requests.get(url)
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+
+        return None 
+    html = BeautifulSoup(page.text, 'html.parser')
+    classToDig = html.find("div",class_="Lyrics__Container-sc-1ynbvzw-6 lgZgEN")
+    if classToDig is None:
+        return None
+    else:
+        lyrics = classToDig.get_text()
+    #remove identifiers like chorus, verse, etc
+    lyrics = re.sub(r'[\(\[].*?[\)\]]', '', lyrics)
+    #remove empty lines
+    lyrics = os.linesep.join([s for s in lyrics.splitlines() if s])
+    lyrics = re.sub(r'[^\w]', ' ', lyrics) 
+    words = lyrics.split(" ")
+    allWords = []
+    for word in words:
+        res_list = [s for s in re.split("([A-Z][^A-Z]*)", word) if s]
+        allWords += res_list
+    return " ".join(allWords)
+
+def scrape_lyrics_song(artist, song):
+    initialStr = "https://genius.com/"
+    replaceArtist = artist.replace(" ","-")
+    replaceName = song.replace(" ", "-")
+    newStr = initialStr + replaceArtist + "-" + replaceName + "-lyrics"
+    return scrape_song_lyrics(newStr)
+
 # If a song name is given, it returns the closest song to it; otherwise it predicts randomly
 def main():
     dataNp = pd.read_csv('../data/songsToMeaningAndLyricsReal.csv')
     # removing lyrics with less than 20 words and more than 100 words
-    rows = dataNp.index[[72, 81, 132, 133, 427, 429, 496, 530, 551, 580, 583, 684, 705, 721, 921, 926, 930, 934, 955, 1015, 1063, 1075, 1144, 1169, 1193, 1205, 1209, 1296, 1301, 1334, 1338, 1357, 1365, 1376, 1415, 1417, 1431, 1482, 1488, 1490, 1493, 1498, 1500, 1501, 1515, 1541, 1549, 1576, 1577, 1589, 1591, 1601, 1679, 1684, 1701, 1706, 1735, 1763, 1770, 1796, 1840, 1846, 1847, 1864, 1882, 1925, 1934, 1939, 1944, 1955, 1957, 2020, 2026, 2054, 2096, 2105, 2108]]
+    drop_rows=[72, 81, 132, 133, 427, 429, 496, 530, 551, 580, 583, 684, 705, 721, 921, 926, 930, 934, 955, 1015, 1063, 1075, 1144, 1169, 1193, 1205, 1209, 1296, 1301, 1334, 1338, 1357, 1365, 1376, 1415, 1417, 1431, 1482, 1488, 1490, 1493, 1498, 1500, 1501, 1515, 1541, 1549, 1576, 1577, 1589, 1591, 1601, 1679, 1684, 1701, 1706, 1735, 1763, 1770, 1796, 1840, 1846, 1847, 1864, 1882, 1925, 1934, 1939, 1944, 1955, 1957, 2020, 2026, 2054, 2096, 2105, 2108]
+    rows = dataNp.index[drop_rows]
+
     dataNp.drop(rows, inplace=True)
     dataNp = np.array(dataNp)
+
+    for i in range(len(dataNp)):
+        dataNp[i][0] = i
+
     songs, song_word_counts, word_ids, labels, id_to_name, name_to_id = preprocess_lyrics(dataNp)
     if len(sys.argv) == 2:
         song_name = sys.argv[1]
         if song_name in name_to_id:
             song_ids = [name_to_id[song_name]]
+        else: 
+            print("could not find song")
+            exit()
     else:
         song_ids = np.random.choice(np.arange(2102), size=10)
     tf_idf_matrix = tf_idf(songs, song_word_counts, word_ids)
@@ -109,7 +160,6 @@ def main():
             control_count += 1
         print("input: ", id_to_name[i], " recommendation: ", id_to_name[similar_song_id])
         print("input label: ", labels[i], " recommendation label: ", labels[similar_song_id])
-        print("ids: ", i, similar_song_id)
 
     print("accuracy: ", count / len(song_ids))
     print("% breakup and love were mixed: ", control_count/ len(song_ids))
